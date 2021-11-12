11b149bc3e48e365e0a1c50deb0bf3cd9dc145e7
joshuaphilips <philipsjoshua96@gmail.com>
2021-11-12 15:07:47 +0000

    book controller and display book name

---
+++ src/main/java/io/joshuaphilips/betterreads/book/BookRepository.java
@@ -0,0 +1,10 @@
+package io.joshuaphilips.betterreads.book;
+
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CassandraRepository<Book, String> {
+
+}
