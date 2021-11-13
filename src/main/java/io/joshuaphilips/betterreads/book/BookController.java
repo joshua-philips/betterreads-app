54b43f7d4a3f472e85d0b4cdda6fa548ffed66a2
joshuaphilips <philipsjoshua96@gmail.com>
2021-11-13 18:24:46 +0000

    search: controller, classes, templates

--- src/main/java/io/joshuaphilips/betterreads/book/BookController.java
+++ src/main/java/io/joshuaphilips/betterreads/book/BookController.java
@@ -22,7 +22,7 @@ public class BookController {
         if (optionalBook.isPresent()) {
             Book book = optionalBook.get();
             String coverImageUrl = "/images/no-image.jpg";
-            if (book.getCoverIds() != null & book.getCoverIds().size() > 0) {
+            if (book.getCoverIds() != null && book.getCoverIds().size() > 0) {
                 coverImageUrl = COVER_IMAGE_ROOT + book.getCoverIds().get(0) + "-L.jpg";
             }
             model.addAttribute("coverImage", coverImageUrl);
